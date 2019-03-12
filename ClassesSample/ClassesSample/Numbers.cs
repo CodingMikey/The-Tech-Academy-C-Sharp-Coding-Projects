638255ff10871daf210681673a02b5280697dd40
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-03-12 12:33:39 -0700

    ClassesExercise
    ClassesExercise

---
+++ ClassesSample/ClassesSample/Numbers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesSample
+{
+    class Numbers
+    {
+        private int _firstNumber;
+        private int _secondNumber;
+        private int _thirdNumber;
+
+        public Numbers(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            _firstNumber = firstNumber;
+            _secondNumber = secondNumber;
+            _thirdNumber = thirdNumber;
+        }
+
+        public Numbers()
+        {
+            _firstNumber = 5;
+            _secondNumber = 10;
+            _thirdNumber = 25;
+        }
+
+        public int firstNumber
+        {
+            set
+            {
+                _firstNumber = value;
+            }
+            get
+            {
+                return _firstNumber;
+            }
+        }
+        public int secondNumber
+        {
+            set
+            {
+                _secondNumber = value;
+            }
+            get
+            {
+                return _secondNumber;
+            }
+        }
+        public int thirdNumber
+        {
+            set
+            {
+                _thirdNumber = value;
+            }
+            get
+            {
+                return _thirdNumber;
+            }
+        }
+        public int multiply()
+        {
+            return _firstNumber * _secondNumber;
+        }
+        public int multiplyTwo()
+        {
+            return _secondNumber * _secondNumber;
+        }
+        public int multiplyThree()
+        {
+            return _thirdNumber * _secondNumber;
+        }
+
+    }
+}
+
---
+++ ClassesSample/ClassesSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers usersInput = new Numbers();
+            string numbers = " 1.) 5 x 10 \n 2.) 10 x 10 \n 3.) 25 x 10";
+
+            Console.WriteLine(numbers);
+            Console.WriteLine("what number do you want to do the math operations on?");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (inputNumber == 1)
+            {
+                Console.WriteLine(usersInput.multiply().ToString());
+                Console.ReadLine();
+            }
+            else if (inputNumber == 2)
+            {
+                Console.WriteLine(usersInput.multiplyTwo().ToString());
+                Console.ReadLine();
+            }
+            else if (inputNumber == 3)
+            {
+                Console.WriteLine(usersInput.multiplyThree().ToString());
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please pick between 1-3");
+                Console.ReadLine();
+            }
+        }
+        
+    }
+}
