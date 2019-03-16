010f13577172ad3a01c3d8688f1f76b3733c52fa
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-03-16 13:14:02 -0700

    VoidMethod
    VoidMethod

---
+++ VoidMethod/VoidMethod/Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Numbers
+    {
+        public void numbers(int num1, int num2)
+        {
+            num1 = +10;
+            num2 = 25;
+        }
+    }
+}
