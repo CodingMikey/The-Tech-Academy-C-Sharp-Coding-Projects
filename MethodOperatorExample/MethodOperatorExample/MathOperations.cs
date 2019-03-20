1fe25d64508f0385267f561903e9ede60c884f22
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-03-19 17:15:37 -0700

    Classes and Methods
    Classes and Methods

---
+++ MethodOperatorExample/MethodOperatorExample/MathOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOperatorExample
+{
+    class MathOperations
+    {
+        public int addition(int num1)
+        {
+            int results = num1 + 5;
+
+            return results;
+        }
+        public decimal addition(decimal num1)
+        {
+            decimal results = num1 / 99;
+
+            return results;
+        }
+        public int addition(string num1)
+        {
+            int results = Convert.ToInt32(num1) + 15;
+            
+
+            return results;
+        }
+
+    }
+}
+
---
+++ MethodOperatorExample/MethodOperatorExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOperatorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperations = new MathOperations();
+            int num1 = 88;
+            
+
+
+            int resultOne = mathOperations.addition(num1);
+            
+
+            decimal resultTwo = mathOperations.addition(15m);
+            
+
+            int resultThree = mathOperations.addition("15");
+            Console.WriteLine("{0} - {1} - {2}", resultOne, resultTwo, resultThree);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
