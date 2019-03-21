9c25f6c9552156930158f057c8b08826d0f0b824
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-03-20 20:01:44 -0700

    Optional Parameter
    Optional Parameter

---
+++ OptionalParameter/OptionalParameter/Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Numbers
+    {
+        public int numbersClass(int num1, int num2 = 0)
+        {
+            int results = num1 + num2 + 10;
+
+            return results;
+        }
+
+    }
+}
---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine("Enter two numbers, one at a time. The second number is optional");
+            int usersInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number or enter zero");
+            int usersInputTwo = Convert.ToInt32(Console.ReadLine());
+
+            int oneNumber = numbers.numbersClass(usersInput, usersInputTwo);
+            int twoNumbers = numbers.numbersClass(usersInput, usersInputTwo);
+
+             if (usersInputTwo == 0)
+            {
+                Console.WriteLine(oneNumber);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(twoNumbers);
+                Console.ReadLine();
+            }
+        }
+    }
+}
