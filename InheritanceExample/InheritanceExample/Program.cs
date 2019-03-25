c742393bf8616116a85ae2784cbd3083ee4e1314
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-03-24 18:43:16 -0700

    Inheritance and super class method
    Inheritance and super class method

---
+++ InheritanceExample/InheritanceExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
