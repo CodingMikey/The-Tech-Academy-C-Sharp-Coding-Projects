ea131d5d1bc9e229d309200e5c68eb6ff351f700
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-03-24 17:40:43 -0700

    Method Overloads and Output Parameters
    Method Overloads and Output Parameters

---
+++ OverloadingMethod/OverloadingMethod/ClassTwo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethod
+{
+    static class ClassTwo
+    {
+        public static string fName = "Mike";
+        public static string lName = "Galas";
+    }
+}
