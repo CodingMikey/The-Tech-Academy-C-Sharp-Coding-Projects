849df6d043471d2a28d4d18915816f6e93988516
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-09 10:43:30 -0700

    Enum Exercise
    Enum Exercise

---
+++ ExerciseEnum/ExerciseEnum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter the day of the week.");
+
+                var enumAsName = Console.ReadLine();
+                var enumFromString = Enum.Parse(typeof(weekDays), enumAsName);
+                
+
+
+                Console.WriteLine(enumFromString);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+               
+            }
+        
+        }
+        public enum weekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
