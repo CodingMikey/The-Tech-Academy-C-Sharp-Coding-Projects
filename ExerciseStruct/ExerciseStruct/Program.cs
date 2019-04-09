9e0ea0deecc56a48bb798d170774343f3eeb928f
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-09 11:41:40 -0700

    Struct Example
    Struct Example

---
+++ ExerciseStruct/ExerciseStruct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+
+            number.Amount = 7.5m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
