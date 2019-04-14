6888142e6d74856afdd1fc7f2af72bc297dba92d
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-13 21:31:08 -0700

    I/O Exercise
    Getting a users input then creating a new text file with the users input

---
+++ IOExercise/IOExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\MeMe\Desktop\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+        }
+    }
+}
