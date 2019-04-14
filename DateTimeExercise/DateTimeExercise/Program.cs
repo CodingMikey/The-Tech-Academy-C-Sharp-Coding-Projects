5a3e3fae13be93e38b0b3e021cb30031193898f9
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-13 22:04:22 -0700

    DateTime Exercise
    DateTime Exercise

---
+++ DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            Console.WriteLine(dateTime);
+            Console.WriteLine("Enter a number");
+            int usersNumber = Convert.ToInt32(Console.ReadLine());
+            DateTime combined = dateTime.AddHours(usersNumber);
+
+            Console.WriteLine("You entered {0} it will be {1} in {0} hours", usersNumber, combined);
+            Console.ReadLine();
+        }
+    }
+}
