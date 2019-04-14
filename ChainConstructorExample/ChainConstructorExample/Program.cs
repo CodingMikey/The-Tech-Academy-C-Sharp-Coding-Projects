3fd6a8e1320aadfb045144bedca87de9a81e69d5
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-14 13:13:55 -0700

    Constructor Chaining
    EXERCISE:
    1. Create a const variable
    
    2. Create a variable using the keyword "var".
    
    3. Chain two constructors together.

---
+++ ChainConstructorExample/ChainConstructorExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainConstructorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Coding Mikey!";
+
+            var myAge = 29;
+
+            Person person = new Person("Mike", 5);
+
+
+
+            Console.WriteLine(person);
+            Console.ReadLine();
+
+        }
+    }
+    public class Person
+    {
+
+        public Person(string name) : this(name, 100)
+        {
+
+        }
+        public Person(string name, int age)
+        {
+            age = age;
+            name = name;
+
+        }
+        
+        
+    }
+    
+}
