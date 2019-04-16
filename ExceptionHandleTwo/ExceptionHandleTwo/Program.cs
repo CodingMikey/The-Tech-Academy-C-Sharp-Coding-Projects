9cd5db72f4c08877cb0eb80645f5c9386c1de457
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-15 21:31:20 -0700

    Exception Handling & DateTime
    Exception Handling & DateTime

---
+++ ExceptionHandleTwo/ExceptionHandleTwo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandleTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter in your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+
+                DateTime aTime = DateTime.Now;
+
+
+                Console.WriteLine("You were born in the year: {0} ", aTime.Year - age);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("Please type a positive.");
+                
+                return;
+
+            }
+            
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
