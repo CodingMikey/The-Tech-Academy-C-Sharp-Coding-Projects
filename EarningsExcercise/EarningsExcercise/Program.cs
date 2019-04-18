a989ecb47618bf229b2a50e5cf5183c50e98ddf1
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-04-17 19:03:15 -0700

    Simple Earnings Calculator
    Simple Earnings Calculator

---
+++ EarningsExcercise/EarningsExcercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarningsExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles did you drive today?");
+            double miles = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many stops did you make today?");
+            double stops = Convert.ToInt32(Console.ReadLine());
+
+            double todaysPay = (miles * 0.48) + stops * 18.50;
+
+            Console.WriteLine("You made ${0} today!", todaysPay);
+            Console.ReadLine();
+        }
+    }
+}
